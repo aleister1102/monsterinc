1f00d8ef465aa8eb398bf9d771f71ddfda092b69
quan <marucube35@gmail.com>
2025-05-29 15:30:17 +0700

    feat(models): add interfaces for validation, timestamping, identification, serialization, and content management

---
+++ internal/models/interfaces.go
@@ -0,0 +1,70 @@
+package models
+
+import "time"
+
+// Validator interface for models that can validate themselves
+type Validator interface {
+	Validate() error
+}
+
+// Timestamped interface for models that have timestamp information
+type Timestamped interface {
+	GetTimestamp() time.Time
+}
+
+// Identifiable interface for models that have unique identifiers
+type Identifiable interface {
+	GetID() string
+}
+
+// Serializable interface for models that can serialize themselves
+type Serializable interface {
+	ToJSON() ([]byte, error)
+	FromJSON([]byte) error
+}
+
+// StatusProvider interface for models that have status information
+type StatusProvider interface {
+	GetStatus() string
+	SetStatus(string)
+}
+
+// Counter interface for models that can count specific items
+type Counter interface {
+	Count() int
+}
+
+// URLProvider interface for models that contain URL information
+type URLProvider interface {
+	GetURL() string
+}
+
+// ContentProvider interface for models that contain content
+type ContentProvider interface {
+	GetContent() []byte
+	GetContentType() string
+}
+
+// ErrorProvider interface for models that can contain error information
+type ErrorProvider interface {
+	GetError() string
+	HasError() bool
+}
+
+// TechnologyDetector interface for models that can detect technologies
+type TechnologyDetector interface {
+	HasTechnologies() bool
+	GetTechnologies() []Technology
+}
+
+// DiffResult interface for models that represent diff results
+type DiffResult interface {
+	GetDiffs() interface{}
+	IsIdentical() bool
+}
+
+// SecretContainer interface for models that can contain secret findings
+type SecretContainer interface {
+	GetSecretFindings() []SecretFinding
+	HasSecrets() bool
+}
