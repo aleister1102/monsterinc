43cd4d59fd23baee0b1154b1380b0f035a02fa74
quan <marucube35@gmail.com>
2025-06-01 16:18:01 +0700

    feat(regex): add CompileRegexes function to compile and log regex patterns

---
+++ internal/common/regex_utils.go
@@ -0,0 +1,31 @@
+package common
+
+import (
+	"regexp"
+
+	"github.com/rs/zerolog"
+)
+
+// CompileRegexes compiles a slice of regex strings into a slice of *regexp.Regexp.
+// It logs an error for any regex that fails to compile but continues with others.
+func CompileRegexes(
+	regexStrings []string,
+	logger zerolog.Logger,
+) []*regexp.Regexp {
+	var compiledRegexes []*regexp.Regexp
+	if len(regexStrings) == 0 {
+		logger.Debug().Msg("No regex strings provided for compilation.")
+		return compiledRegexes
+	}
+
+	for _, regexStr := range regexStrings {
+		re, err := regexp.Compile(regexStr)
+		if err != nil {
+			logger.Error().Err(err).Str("regex", regexStr).Msg("Failed to compile regex, skipping.")
+		} else {
+			compiledRegexes = append(compiledRegexes, re)
+		}
+	}
+	logger.Debug().Int("input_count", len(regexStrings)).Int("compiled_count", len(compiledRegexes)).Msg("Finished compiling regex strings.")
+	return compiledRegexes
+}
